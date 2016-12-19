7f6fb444fc868ccde974f2e88373a9f516e6f227
BernardJeremy <jeremy@bernard.sh>
2016-12-19 17:23:38 +0100

    add eslint + use ES6 JS

--- README.md
+++ README.md
@@ -5,18 +5,19 @@ Node.JS script allowing to send Epitech intranet's messages to Slack.
 ## Features
 - Retrieves messages from Epitech's intranet
 - Send the title, content and date of any new messages to a configured Slack hook
+- Send any new module to the same configured Slack hook
 - Replace potential HTML link by a Slack compliant version
 
 ## Installation
 - Simply clone this depot anywhere on your server.
-- Copy [config.json.example](https://github.com/DUDU54330/intra-slack/blob/master/config.json.example) file into a `config.json` file.
+- Copy [config.json.example](https://github.com/BernardJeremy/intra-slack/blob/master/config.json.example) file into a `config.json` file.
 - Add your intranet login/password to the `config.json` file.
 - Install a [incoming-webhooks](https://api.slack.com/incoming-webhooks) on your Slack.
 - Add your link of the Slack incoming-webhooks in the `config.json` file.
 - Optional (but recommended) : Install a task scheduler (like `CRON`) to run the script regularly.
 
 ## Configuration
-- `login` : Your intranet login.
+- `login` : Your intranet login/email.
 - `password` : Your intranet password.
 - `hostname` : Hostname of the intranet API to retrieve all messages (You shouldn't have to change this).
 - `path` : Path of the intranet API to retrieve all messages (You shouldn't have to change this).
--- index.js
+++ index.js
@@ -1,145 +1,148 @@
 const fs = require('fs');
-const https = require('https')
+const https = require('https');
 const Slack = require('slack-node');
 const slackify = require('slackify-html');
 
 // Retrieve config
-var config = require('./config.json');
+const config = require('./config.json');
 
 // Send the slack message to the config's webhook
 function sendSlackMessage(text, content, date) {
-	var msgParameters = {
-		username: config.slackHookName,
-		text: text,
-	};
-	if (content !== null && date !== null) {
-		msgParameters.attachments = [
-			{
-				color: "#015a9f",
-				fields:[
-					{
-						title:date,
-						value:content,
-						short:false
-					}
-				]
-			}
-		]
-	}
-	var slack = new Slack();
-	slack.setWebhook(config.slackHookUrl);
-	slack.webhook(msgParameters, function (err, response) {
-		if (response.statusCode !== 200) {
-			console.log(err, response);
-		}
-	});
+  const msgParameters = {
+    username: config.slackHookName,
+    text,
+  };
+  if (content !== null && date !== null) {
+    msgParameters.attachments = [
+      {
+        color: '#015a9f',
+        fields: [
+          {
+            title: date,
+            value: content,
+            short: false,
+          },
+        ],
+      },
+    ];
+  }
+  const slack = new Slack();
+  slack.setWebhook(config.slackHookUrl);
+  slack.webhook(msgParameters, (err, response) => {
+    if (response.statusCode !== 200) {
+      console.log(err, response);
+    }
+  });
 }
 
 // return filename
 function getSaveFilename(type) {
-	return 'savedID_intra_' + type + '.json'
+  return `savedID_intra_${type}.json`;
 }
 
 // Perform update of the save data file
 function updateSavedData(savedData, type, newID) {
-	savedData[newID] = true;
-	if (fs.existsSync(getSaveFilename(type))) {
-		fs.unlinkSync(getSaveFilename(type));
-	}
-	fs.writeFileSync(getSaveFilename(type), JSON.stringify(savedData));
+  const newSavedData = savedData;
+  newSavedData[newID] = true;
+  if (fs.existsSync(getSaveFilename(type))) {
+    fs.unlinkSync(getSaveFilename(type));
+  }
+  fs.writeFileSync(getSaveFilename(type), JSON.stringify(newSavedData));
 }
 
 function decodeHTMLEntities(text) {
-	var entities = [
-		['apos', '\''],
-		['amp', '&'],
-		['lt', '<'],
-		['gt', '>']
-	];
+  const entities = [
+    ['apos', '\''],
+    ['amp', '&'],
+    ['lt', '<'],
+    ['gt', '>'],
+  ];
+  let decodedText = text;
 
-	for (var i = 0, max = entities.length; i < max; ++i)
-		text = text.replace(new RegExp('&'+entities[i][0]+';', 'g'), entities[i][1]);
+  for (let i = 0, max = entities.length; i < max; i += 1) {
+    decodedText = decodedText.replace(new RegExp(`&${entities[i][0]};`, 'g'), entities[i][1]);
+  }
 
-	return text;
+  return decodedText;
 }
 
 // return potential saved data from file
 function getSavedData(type) {
-	var savedData = {};
-	if (fs.existsSync(getSaveFilename(type))) {
-		savedData = JSON.parse(fs.readFileSync(getSaveFilename(type), 'utf8'));
-	}
+  let savedData = {};
+  if (fs.existsSync(getSaveFilename(type))) {
+    savedData = JSON.parse(fs.readFileSync(getSaveFilename(type), 'utf8'));
+  }
 
-	return savedData;
+  return savedData;
 }
 
 // iterate over every messages and send new one to slack
 function messageNotifier(json, type) {
-	var savedData = getSavedData(type);
+  const savedData = getSavedData(type);
 
-	for (var i = 0; i < json.length; i++){
-		var msg = json[i];
-		var id = msg.id;
+  for (let i = 0; i < json.length; i += 1) {
+    const msg = json[i];
+    const id = msg.id;
 
-		if (!(id in savedData)) {
-			var title = decodeHTMLEntities(msg.title);
-			title = title.split('href="').join('href="https://intra.epitech.eu');
-			title = slackify(title);
-			var content = slackify(msg.content);
-			var date = slackify(msg.date);
-			sendSlackMessage(title, content, date);
-			updateSavedData(savedData, type, id);
-		}
-	}
+    if (!(id in savedData)) {
+      let title = decodeHTMLEntities(msg.title);
+      title = title.split('href="').join('href="https://intra.epitech.eu');
+      title = slackify(title);
+      const content = slackify(msg.content);
+      const date = slackify(msg.date);
+      sendSlackMessage(title, content, date);
+      updateSavedData(savedData, type, id);
+    }
+  }
 }
 
 // iterate over every modules and send new one to slack
 function moduleNotifier(json, type) {
-	var savedData = getSavedData(type);
+  const savedData = getSavedData(type);
 
-	for (var i = 0; i < json.length; i++){
-		var module = json[i];
-		var id = module.title_link;
+  for (let i = 0; i < json.length; i += 1) {
+    const module = json[i];
+    const id = module.title_link;
 
-		if (!(id in savedData)) {
-			var title = "*New module available* : ";
-			title += "<" + "https://intra.epitech.eu" + decodeHTMLEntities(module.title_link);
-			title += "|" + decodeHTMLEntities(module.title) + ">";
-			sendSlackMessage(title, null, null);
-			updateSavedData(savedData, type, id);
-		}
-	}
+    if (!(id in savedData)) {
+      let title = '*New module available* : ';
+      title += `<https://intra.epitech.eu${decodeHTMLEntities(module.title_link)}`;
+      title += `|${decodeHTMLEntities(module.title)}>`;
+      sendSlackMessage(title, null, null);
+      updateSavedData(savedData, type, id);
+    }
+  }
 }
 
 function main() {
-	var postBody = '{"login": "'+config.login+'", "password": "'+config.password+'"}';
-	var options = {
-		hostname: config.hostname,
-		path: config.path,
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-	  }
-	};
-	callback = function(res) {
-		var response = '';
-		res.setEncoding('utf8');
-		res.on('data', function (body) {
-			response += body;
-		});
-	 	res.on('end', function () {
-			var json = JSON.parse(response);
-			messageNotifier(json.history, "history");
-			moduleNotifier(json.board.modules, "modules");
-		});
-	};
-	var req = https.request(options, callback);
-	req.on('error', function(e) {
-	  console.log('problem with request: ' + e.message);
-	});
-	// write data to request body
-	req.write(postBody);
-	req.end();
+  const postBody = `{"login": "${config.login}", "password": "${config.password}"}`;
+  const options = {
+    hostname: config.hostname,
+    path: config.path,
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  };
+  const callback = (res) => {
+    let response = '';
+    res.setEncoding('utf8');
+    res.on('data', (body) => {
+      response += body;
+    });
+    res.on('end', () => {
+      const json = JSON.parse(response);
+      messageNotifier(json.history, 'history');
+      moduleNotifier(json.board.modules, 'modules');
+    });
+  };
+  const req = https.request(options, callback);
+  req.on('error', (e) => {
+    console.log(`Problem with intra request: ${e.message}`);
+  });
+  // write data to request body
+  req.write(postBody);
+  req.end();
 }
 
 main();
